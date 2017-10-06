db187cd7ad63c048daff37b88f81af55605e51cd
JONATHAN CRUZ ROCHA FERREIRA <jonathan.ferreira@animaeducacao.com.br>
2017-10-06 15:01:13 -0300

    gitignore

---
+++ src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <div class="teste"> AA</div>
+    <div class="teste-url"> BB</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.teste {
+  color: $auxiliary-color;
+  background-image: url('../assets/icons/ico_atalho.png')
+}
+
+</style>
